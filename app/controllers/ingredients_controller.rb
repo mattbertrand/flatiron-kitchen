9960aa3fe944c3ef55bd337124fa05a6fc21ec51
github username <github email address>
2021-01-21 16:31:08 -0800

    commit tree 99f22eb3f60b36764f604c6eebf819c2d854db29

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,45 @@
+class IngredientsController < ApplicationController
+
+    before_action :set_ingredient, only: [:edit, :update]
+
+    def index
+        @ingredients = Ingredient.all
+    end
+
+    def new
+        @ingredient = Ingredient.new
+    end
+
+    def create
+        ingredient = Ingredient.new(ingredient_params)
+
+        if ingredient.save
+            redirect_to ingredients_path
+        else
+            render :new
+        end
+
+        def edit
+        end
+
+        def update
+            @ingredient.update(ingredient_params)
+
+            if @ingredient.save
+                redirect_to ingredients_path
+            else
+                render :edit
+            end
+        end
+
+    end
+
+    private
+        def ingredient_params
+            params.require(:ingredient).permit(:name)
+        end
+
+        def set_ingredient
+            @ingredient = Ingredient.find(params[:id])
+        end
+end
